9b6f1266aeb530df2550dc6ec131a9aad7c9443e
Ankit Kumar <abhishekankit128@gmail.com>
2024-05-05 11:37:57 +0530

    integrated backend with foodModel,controller and route

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const cors  = require('cors') 
+const connectDB = require('./config/db');
+const foodRouter = require('./routes/foodRoute');
+
+//app config
+const app = express()
+const port = 4000
+
+//middleware
+app.use(express.json())
+app.use(cors()); //access backend from frontend
+
+//db connection
+connectDB(); 
+
+//api endpoints
+app.use("/api/food", foodRouter)
+ 
+
+app.listen(port,()=>{
+    console.log(`App is running on port:${port}`);
+})
